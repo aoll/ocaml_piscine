6f997a07348997b0e6514a3f7fe4c809cbf1d926
aoll <agollivier@gmail.com>
2018-05-30 17:03:55 +0200

    d01 ex06 ex07

--- j01/ex07/converges.ml
+++ j01/ex07/converges.ml
@@ -1,23 +1,31 @@
 (* ************************************************************************** *)
 (*                                                                            *)
 (*                                                        :::      ::::::::   *)
-(*   iter.ml                                            :+:      :+:    :+:   *)
+(*   converges.ml                                       :+:      :+:    :+:   *)
 (*                                                    +:+ +:+         +:+     *)
 (*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
-(*   Created: 2018/05/30 04:43:04 by alex              #+#    #+#             *)
-(*   Updated: 2018/05/30 16:37:04 by alex             ###   ########.fr       *)
+(*   Created: 2018/05/30 16:37:20 by alex              #+#    #+#             *)
+(*   Updated: 2018/05/30 17:02:36 by alex             ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-let iter f x n =
+let converges f x n =
   let rec loop f x n =
-    if n == 0 then x
-    else if n < 0 then -1
-    else loop f (f x) (n - 1)
+    let tmp = f (x) in
+    if  tmp == x then true
+    else if n == 0 then false
+    else loop f tmp (n - 1)
   in
   loop f x n
 
 let () =
-  print_string "iter (fun x -> x * x) 2 4: " ; print_int (iter (fun x -> x * x) 2 4) ; print_string "\n" ;
-  print_string "(iter (fun x -> x * 2) 2 4): " ; print_int (iter (fun x -> x * 2) 2 4) ; print_string "\n"
+  if converges (( * ) 2) 2 5 == true then print_string "true\n"
+  else
+    print_string "false\n" ;
+  if converges (fun x -> x / 2) 2 3 == true then print_string "true\n"
+  else
+    print_string "false\n" ;
+  if converges (fun x -> x / 2) 2 2 then print_string "true\n"
+  else
+    print_string "false\n"
