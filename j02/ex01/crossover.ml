b442451d5d7083eabb76e26c2495b837eb5074df
Alexandre OLLIVIER <aollivie@e1r8p20.42.fr>
2018-05-31 15:49:39 +0200

    print commom elem

---
+++ j02/ex01/crossover.ml
@@ -0,0 +1,18 @@
+let crossover l1 l2 =
+	let rec is_in_list l elem = match l with
+	| [] -> false
+	| head::tail when head = elem -> true
+	| head::tail -> is_in_list tail elem
+	in
+	let rec loop l acc = match l with
+	| [] -> acc
+	| head::tail when is_in_list l2 head -> loop tail (acc @ [head])
+	| head::tail -> loop tail acc
+	in
+	loop l1 []
+
+let () =
+	let p a =
+		print_int a ; print_string "\n"
+		in
+	List.iter p (crossover [5;42;1;3] [1;2;3])
