deebb6fab86e37dfac99022b86681c6d9bdac48e
aoll <agollivier@gmail.com>
2018-06-05 21:56:02 +0200

    started

---
+++ d06/ex00/ex00.ml
@@ -0,0 +1,16 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   ex00.ml                                            :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2018/06/05 16:27:51 by alex              #+#    #+#             *)
+(*   Updated: 2018/06/05 16:27:53 by alex             ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let () =
+  let set = List.fold_right StringSet.add [ "foo"; "bar"; "baz"; "qux" ] StringSet.empty in
+  StringSet.iter print_endline set;
+  print_endline (StringSet.fold ( ^ ) set "")
